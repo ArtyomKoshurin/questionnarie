b08082a659fb9b089acd1b622f561c86a2adab3e
Artyom Koshurin <coshurin.art@yandex.ru>
2026-08-09 18:40:51 +0300

    Started project

---
+++ infra/local/entrypoint.sh
@@ -0,0 +1,16 @@
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+  done
+
+  echo "Postgres started"
+fi
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py runserver 0.0.0.0:8000
+exec "$@"
