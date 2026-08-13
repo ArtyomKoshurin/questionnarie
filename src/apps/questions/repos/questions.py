d68114950cf9df60ee20912cbe9c6301e5eb6d81
Artyom Koshurin <coshurin.art@yandex.ru>
2026-08-13 21:37:28 +0300

    13.08.2026 | Added readme description

--- src/apps/questions/repos/questions.py
+++ src/apps/questions/repos/questions.py
@@ -1,6 +1,7 @@
 from src.apps.questions.domain.entities.questions import Question
 from src.apps.questions.domain.interfaces.repos.questions import \
     BaseQuestionRepo
+from src.apps.questions.exceptions.questions import GetQuestionByOrderException
 from src.apps.questions.models.questions import QuestionModel
 
 
@@ -10,10 +11,9 @@ class QuestionRepo(BaseQuestionRepo):
         ...
 
     def get_by_questionnaire_and_order(self, questionnaire_uuid: str, order_number: int):
-        # try:
-        obj = QuestionModel.objects.get(questionnaire__uuid=questionnaire_uuid, order_number=order_number)
-        return obj.to_entity()
+        try:
+            obj = QuestionModel.objects.get(questionnaire__uuid=questionnaire_uuid, order_number=order_number)
+            return obj.to_entity()
 
-        # except Exception:
-        #     traceback.print_exc(limit=10)
-        #     raise GetQuestionByOrderException(question=order_number)
+        except Exception:
+            raise GetQuestionByOrderException(question=order_number)
